3dede4131363ab0d06bc09c6476d137d779f926c
Victoria Blackburn <victoriablackburn@Victorias-MacBook-Air.local>
2020-07-28 21:01:09 -0700

    updated files

--- assets/script.js
+++ assets/script.js
@@ -5,26 +5,33 @@ var choices2= document.querySelector("choices2");
 var choice= document.querySelector(".choice");
 var counter= document.querySelector("#counter");
 var secondsLeft= 10;
-console.log("sdkjhskjhdjkshsdkj")
+
 startBtn.addEventListener("click", function() {
     console.log(startBtn);
     var timeInterval = setInterval(function() {
         secondsLeft--;
         counter.textContent= secondsLeft + "seconds left till quiz over";
+        if(secondsLeft===0){
+            clearInterval(timeInterval);
+            alert("You are out of time!");
+        }
+        
         console.log(secondsLeft);
-    }, 1000)
-    choice.addEventListener("click", function(){   
+        choice.addEventListener("click", function(){   
 
-    if(secondsLeft===0 && choices1!=='D' || choices2!=='C'){
-        clearInterval(timeInterval);
-        alert("Wrong answer, you are out of time");
-        console.log(choices1);
-        console.log(choices2);
-        }    
+            if(secondsLeft===0 && choices1!=='D' || choices2!=='C'){
+                clearInterval(timeInterval);
+                alert("Wrong answer, you are out of time");
+                console.log(choices1);
+                console.log(choices2);
+                }  
+                if( choices1=='D' && choices2=='C'){
+                    alert("You are correct!");
+                    prompt("Enter your intials and high score:");
+                    };
+                    })    
+    }, 1000)
+     
     
-   if( choices1=='D' && choices2=='C'){
-    alert("You are correct!");
-    prompt("Enter your intials and high score:");
-    };
-    })
+   
 })
