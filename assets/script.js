64b60e627c33d94a3fcbc7a4d2dbd85a04f4aab2
Victoria Blackburn <victoriablackburn@Victorias-MacBook-Air.local>
2020-07-28 20:01:26 -0700

    updated code

--- assets/script.js
+++ assets/script.js
@@ -5,13 +5,14 @@ var choices2= document.querySelector("choices2");
 var choice= document.querySelector(".choice");
 var counter= document.querySelector("#counter");
 var secondsLeft= 10;
-
+console.log("sdkjhskjhdjkshsdkj")
 startBtn.addEventListener("click", function() {
+    console.log(startBtn);
     var timeInterval = setInterval(function() {
         secondsLeft--;
         counter.textContent= secondsLeft + "seconds left till quiz over";
         console.log(secondsLeft);
-    
+    }, 1000)
     choice.addEventListener("click", function(){   
 
     if(secondsLeft===0 && choices1!=='D' || choices2!=='C'){
@@ -20,9 +21,10 @@ startBtn.addEventListener("click", function() {
         console.log(choices1);
         console.log(choices2);
         }    
-    }, 1000);
+    
    if( choices1=='D' && choices2=='C'){
     alert("You are correct!");
     prompt("Enter your intials and high score:");
     };
-}
+    })
+})
